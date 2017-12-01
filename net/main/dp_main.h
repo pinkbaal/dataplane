8eaefc8df29b4d9ce3be1836c31a0036029e7f59
pinkbaal <pinkbaal@sina.com>
2017-12-02 01:54:31 +0800

    头文件编译错误

--- net/main/dp_main.h
+++ net/main/dp_main.h
@@ -9,7 +9,7 @@ struct dp_lcore_param {
 	u16 nb_tx_port;
 	u16 rx_queue_id[RTE_MAX_ETHPORTS];
 	u16 tx_queue_id[RTE_MAX_ETHPORTS];
-}
+};
 
 struct dp_param {
 	struct dp_lcore_param lcore_param[RTE_MAX_LCORE];
